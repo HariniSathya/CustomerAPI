3d43ea04a76a3b17488c83e8f35c75cd783dc041
Harini Sathya <harinisathya17@gmail.com>
2024-12-11 22:40:56 -0600

    added logger in controller class

--- src/main/java/com/example/CustomerAPI/controller/CustomerController.java
+++ src/main/java/com/example/CustomerAPI/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.CustomerAPI.controller;
 import com.example.CustomerAPI.entity.CustomerEntity;
 import com.example.CustomerAPI.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,20 +18,36 @@ import java.util.Optional;
 @RequestMapping(path="/customer-service")
 public class CustomerController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+
     @Autowired
     private final CustomerService customerService;
     @PostMapping(path="/save")
     public ResponseEntity<CustomerEntity> createCustomer(@RequestBody CustomerEntity customer){
-        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+        CustomerEntity savedCustomer = customerService.createCustomer(customer);
+        logger.info("Customer created with ID: {}", savedCustomer.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
     }
 
     @GetMapping(path="/getCustomer")
     public ResponseEntity<Optional<CustomerEntity>> getCustomerById(@RequestParam final String id){
+
+        Optional<CustomerEntity> customer= customerService.findByCustomerId(id);
+
+        if (customer.isPresent()) {
+            logger.info("Customer found with ID: {}", id);
+        } else {
+            logger.warn("Customer not found with ID: {}", id);
+        }
+
         return ResponseEntity.ok(customerService.findByCustomerId(id));
     }
 
     @GetMapping(path="/getAllCustomers")
     public  ResponseEntity<List<CustomerEntity>> getAllCustomers(){
+        for (CustomerEntity customer : customerService.findAllCustomers()) {
+            logger.info("Customer List "+ customer.toString());
+        }
         return ResponseEntity.ok(customerService.findAllCustomers());
     }
 
@@ -37,13 +55,19 @@ public class CustomerController {
     public ResponseEntity<CustomerEntity> updateCustomer(@RequestBody CustomerEntity customer){
         CustomerEntity updatedCustomer = customerService.updateCustomer(customer);
         if (updatedCustomer == null) {
+            logger.info("Customer with ID "+ customer.getId()+" is not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(null);
         }
-        return ResponseEntity.ok(updatedCustomer);    }
+        logger.info("Updated Customer Details "+ updatedCustomer.toString());
+
+        return ResponseEntity.ok(updatedCustomer);
+    }
 
     @DeleteMapping(path="deleteCustomer")
     public ResponseEntity<String> deleteCustomer(@RequestParam final String id){
-        return ResponseEntity.ok(customerService.deleteCustomer(id));
+        String deletedMessage = customerService.deleteCustomer(id);
+        logger.info(deletedMessage);
+        return ResponseEntity.ok(deletedMessage);
     }
 }
